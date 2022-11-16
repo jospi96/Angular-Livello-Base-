f6bfb9f184bc4c0bd0c248a2c5621afcf41f9324
jospi96 <113528331+jospi96@users.noreply.github.com>
2022-11-16 15:24:24 +0100

    3 files modified, 1 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent  {
-
-
+export class CartComponent {
+  items = this.cartService.getItems();
+  constructor(private cartService: CartService) {}
 }
